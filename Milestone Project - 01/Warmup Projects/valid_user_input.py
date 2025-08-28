6e1d574276f7ae3461e969f82316ff279c40a829
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2025-08-28 17:38:50 +0530

    Changed two files

---
+++ Milestone Project - 01/Warmup Projects/valid_user_input.py
@@ -0,0 +1,4 @@
+# VALIDATING USER INPUT
+# Check that input is valid before attempting to convert.
+user_choice = input("Enter a number: ")
+if type(user_choice) == type(str)
--- learning_practice/statements.py
+++ learning_practice/statements.py
@@ -1,11 +1,6 @@
-user_input = input()
-user_input_list = list(user_input)
-print(user_input)
-list_reverse = user_input_list[::-1]
-print(list_reverse)
-reversed_word = ''.join(list_reverse)
-print(reversed_word)
-if user_input == reversed_word:
-    print("Palindrome")
-else:
-    print("Not Palindrome")
+a = int(input())
+# x = 1
+for i in range(1,a+1):
+    fact = i * (i+1)
+print(fact)
+    
