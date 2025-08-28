3e5c580430be48237a80248fb02d35c6cd64b151
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2025-08-28 17:49:23 +0530

    Worked on Warmip Project 'valid_user_input.py'

--- Milestone Project - 01/Warmup Projects/valid_user_input.py
+++ Milestone Project - 01/Warmup Projects/valid_user_input.py
@@ -1,4 +1,7 @@
 # VALIDATING USER INPUT
 # Check that input is valid before attempting to convert.
 user_choice = input("Enter a number: ")
-if type(user_choice) == type(str)
+if user_choice.isdigit():
+    print("You enter the number:",user_choice)
+else:
+    print("You enter the wrong input. We think you enter the number in words please enter in numerical form.")
