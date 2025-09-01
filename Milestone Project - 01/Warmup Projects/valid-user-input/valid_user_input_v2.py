117c837aad54ff598691228854b294c1dff0e3e9
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2025-09-01 11:41:39 +0530

    Learned how to clear the terminal by running the program

--- Milestone Project - 01/Warmup Projects/valid-user-input/valid_user_input_v2.py
+++ Milestone Project - 01/Warmup Projects/valid-user-input/valid_user_input_v2.py
@@ -1,4 +1,10 @@
 # Now, we are trying to clear the ouput once out input is not valid acc to program.
 import os
 def clear_output():
-    
+    os.system('cls')
+x = int(input("Enter a Number:"))
+while x != 2:
+    x = int(input("Enter a Number:"))
+else:
+    clear_output()
+    print("You are dumb!!")
