c0f35d8f1142ca66c95715479d4c8a48ba8917cd
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2025-09-01 12:45:55 +0530

    Coded the function to print Tic-Tac-Toe Game Board

--- Milestone Project - 01/Main-Project/tic_tac_toe.py
+++ Milestone Project - 01/Main-Project/tic_tac_toe.py
@@ -1,1 +1,14 @@
 # Tic-Tac-Toe Game
+
+# The function to print board of Tic-Tac-Toe Game
+def print_board(board):
+    board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+    print("\n   |   |   ")
+    print(f" {board[7]} | {board[8]} | {board[9]} ")
+    print("___|___|___")
+    print("   |   |   ")
+    print(f" {board[4]} | {board[5]} | {board[6]} ")
+    print("___|___|___")
+    print("   |   |   ")
+    print(f" {board[1]} | {board[2]} | {board[3]} ")
+    print("   |   |   \n")
