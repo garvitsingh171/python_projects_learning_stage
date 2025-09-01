f68c79631be2bf6d10997e6d24a3204fe35245b9
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2025-09-01 12:53:45 +0530

    Added a guide for using the board position

--- Milestone Project - 01/Main-Project/tic_tac_toe.py
+++ Milestone Project - 01/Main-Project/tic_tac_toe.py
@@ -1,7 +1,21 @@
 # Tic-Tac-Toe Game
 
+# Guide
+print("Guide for the position of different blocks in the board.")
+board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+print("\n   |   |   ")
+print(" 7 | 8 | 9 ")
+print("___|___|___")
+print("   |   |   ")
+print(" 4 | 5 | 6 ")
+print("___|___|___")
+print("   |   |   ")
+print(" 1 | 2 | 3 ")
+print("   |   |   \n")
+print("Press these numeric values whenever you asked to choose the position of your X/O for the respective positions. \n")
 # The function to print board of Tic-Tac-Toe Game
 def print_board(board):
+    print("Your Board")
     board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
     print("\n   |   |   ")
     print(f" {board[7]} | {board[8]} | {board[9]} ")
@@ -12,3 +26,4 @@ def print_board(board):
     print("   |   |   ")
     print(f" {board[1]} | {board[2]} | {board[3]} ")
     print("   |   |   \n")
+print_board([])
