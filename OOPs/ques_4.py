b05834297614cc1982823c2ef7179c6dc7f2c793
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2025-10-03 17:18:19 +0530

    Solved subclasses question

--- OOPs/ques_4.py
+++ OOPs/ques_4.py
@@ -1,7 +1,7 @@
 class Shape():
     def __init__(self, shape):
         self.shape = shape
-    class Triangle():
+class Triangle(Shape):
         def __init__(self, side1, side2, side3):
             self.side1 = side1
             self.side2 = side2
@@ -9,20 +9,28 @@ class Shape():
         def perimeter(self):
             return self.side1 + self.side2 + self.side3
         def area(self):
-            s = (self.side1 + self.side2 + self.side3)/2
-            return (s(s-self.side1)(self.side2)(self.side3))**0.5
-    class Cirlce():
+            s = self.perimeter()/2
+            return (s*(s - self.side1)*(s - self.side2)*(s - self.side3))**0.5
+class Cirlce(Shape):
         def __init__(self, radius):
             self.radius = radius
         def perimeter(self):
             return 2 * 3.14 * self.radius
         def area(self):
             return 3.14 * (self.radius ** 2)
-    class Square():
+class Square(Shape):
         def __init__(self, side):
             self.side = side
         def perimeter(self):
             return 4 * self.side
         def area(self):
             return self.side ** 2
+
+t = Triangle(10, 12, 15)
+c = Cirlce(16)
+s = Square(18)
+
+print(t.perimeter(), t.area())
+print(c.perimeter(), c.area())
+print(s.perimeter(), s.area())
         
--- learning_practice/moores_voting_algo.py
+++ learning_practice/moores_voting_algo.py
@@ -1,19 +1,13 @@
 arr = list(map(int, input("Enter the arr: ").split()))
-cand = None
-count = 0
+
+dict = {}
+
 for num in arr:
-    if count == 0:
-        cand = num
-        count += 1
-    elif num == cand:
-        count += 1
-    else:
-        count -= 1
-no = 0
-for num in arr:
-    if num == cand:
-        no += 1
-if (len(arr) // 2) < no:
-    print(cand)
-else:
-    print(-1)
+    if num in dict : 
+        dict[num] += 1
+    else : 
+        dict[num] = 1
+
+
+
+print(dict)
