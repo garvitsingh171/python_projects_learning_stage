1e05441eb61a8b677e0647cacbac65f097ce26b4
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2025-10-04 10:40:12 +0530

    Modified DOJO 4

--- learning_practice/dojo4.py
+++ learning_practice/dojo4.py
@@ -1,2 +1,11 @@
-print(hash('apple'))  # Gives an integer
-print(hash('mango'))
+arr = [1, 2, 4, 3]
+n = len(arr)
+for i in range(n - 1):  # outer loop
+    swapped = False
+    for j in range(n - i - 1):  # inner loop
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]  # swap
+            swapped = True
+    if not swapped:
+        break  # stop early if no swaps
+print(arr)
