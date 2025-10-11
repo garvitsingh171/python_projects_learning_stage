829ab0c3cafef58e4267b38ce33d80c40dcff233
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2025-10-11 10:50:56 +0530

    Added Linked List's first file

---
+++ Linked List/linked_list_1.py
@@ -0,0 +1,25 @@
+class Node:
+
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+node1 = Node(12)
+node2 = Node(13)
+node3 = Node(14)
+node4 = Node(15)
+node5 = Node(16)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+current = node1
+
+while current is not None:
+    print(current.data, end=' -> ')
+    current = current.next
+print(None)
+
+
