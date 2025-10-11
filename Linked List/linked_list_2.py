86ebe853aa6bdd669f82318fc9652c23a0694e9b
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2025-10-11 10:59:33 +0530

    worked on printing count of nodes

---
+++ Linked List/linked_list_2.py
@@ -0,0 +1,26 @@
+class Node:
+
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+node1 = Node(12)
+node2 = Node(13)
+node3 = Node(14)
+node4 = Node(15)
+node5 = Node(16)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+current = node1
+
+count = 0
+
+while current is not None:
+    count += 1
+    current = current.next
+
+print(count)
