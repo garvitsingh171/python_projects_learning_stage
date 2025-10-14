9353b0d0fc5c803c97885f7979fea96d8d6a307b
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2025-10-14 14:57:34 +0530

    learned to find the occurences of a value

---
+++ Linked List/count_occurences_of_a_value.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+node1 = Node(11)
+node2 = Node(12)
+node3 = Node(12)
+node4 = Node(14)
+node5 = Node(12)
+node6 = Node(16)
+node7 = Node(17)
+node8 = Node(16)
+node9 = Node(12)
+node10 = Node(19)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+node6.next = node7
+node7.next = node8
+node8.next = node9
+node9.next = node10
+
+current = node1
+
+target = 12
+
+count = 0
+
+while current is not None:
+    if current.data == target:
+        count += 1
+    current = current.next
+print(count)
