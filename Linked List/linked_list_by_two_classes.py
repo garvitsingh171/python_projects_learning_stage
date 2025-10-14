f6097a354beac76795666524fcffd67df0b8ca36
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2025-10-14 15:48:33 +0530

    learned to create linked list by using two classes

---
+++ Linked List/linked_list_by_two_classes.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=' -> ')
+            current = current.next
+        print('None')
+
+my_list = LinkedList()
+
+my_list.insert_at_end(11)
+my_list.insert_at_end(12)
+
+my_list.display()
